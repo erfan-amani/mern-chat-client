5be39c0f519a4197421f1efcc5547d4da6a51405
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-06-08 19:10:44 +0330

    style message section - not complete

--- src/pages/Chat/components/Messages.jsx
+++ src/pages/Chat/components/Messages.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import {
+  PhoneIcon,
+  VideoCameraIcon,
+  EllipsisVerticalIcon,
+} from "@heroicons/react/24/outline";
+import Avatar from "@/components/Avatar";
 
 const Messages = () => {
-  return <div>Messages</div>;
+  return (
+    <div className="flex flex-col h-full">
+      <div className="p-4 border-b-2 border-indigo-100 w-full">
+        <div className="flex items-center justify-between">
+          <Avatar user={{ username: "Your contact" }} withDetail />
+
+          <div className="flex gap-5 items-center">
+            <button>
+              <PhoneIcon className="w-5 h-5" />
+            </button>
+            <button>
+              <VideoCameraIcon className="w-5 h-5" />
+            </button>
+            <button>
+              <EllipsisVerticalIcon className="w-5 h-5" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 flex-1"></div>
+    </div>
+  );
 };
 
 export default Messages;
