6f920f78112770e10cda373a0e3305696b75b82a
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-06-08 15:26:43 +0330

    connect to socket with token

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+import { useSelector } from "react-redux";
+
 import Messages from "./components/Messages";
 import SendMessage from "./components/SendMessage";
 import Sidbar from "./components/Sidbar";
+import { BASE_URL } from "@/library/config";
 
 const Chat = () => {
+  const token = useSelector(state => state.auth.accessToken);
+
+  useEffect(() => {
+    const socket = io(BASE_URL, {
+      auth: {
+        token,
+      },
+    });
+
+    socket.on("connect_error", err => {
+      console.log(err.message);
+    });
+  }, []);
+
   return (
     <div className="flex w-screen h-screen">
       <div className="w-[200px] bg-indigo-50 p-4">
