e4550dedbb7ef0582a905583ee24e28fde0ca4b6
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-06-09 11:54:45 +0330

    getting selected user in message section

--- src/pages/Chat/components/Messages.jsx
+++ src/pages/Chat/components/Messages.jsx
@@ -1,17 +1,33 @@
-import React from "react";
 import {
   PhoneIcon,
   VideoCameraIcon,
   EllipsisVerticalIcon,
 } from "@heroicons/react/24/outline";
 import Avatar from "@/components/Avatar";
+import { useSearchParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "@/library/http";
 
 const Messages = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [activeUser, setActiveUser] = useState({});
+  const selectedUserId = searchParams.get("id");
+
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await axios.get(`user/${selectedUserId}`);
+      const data = response.data;
+      setActiveUser(data);
+    };
+
+    getUser();
+  }, [selectedUserId]);
+
   return (
     <div className="flex flex-col h-full">
       <div className="p-4 border-b-2 border-indigo-100 w-full">
         <div className="flex items-center justify-between">
-          <Avatar user={{ username: "Your contact" }} withDetail />
+          <Avatar user={activeUser} withDetail />
 
           <div className="flex gap-5 items-center">
             <button>
