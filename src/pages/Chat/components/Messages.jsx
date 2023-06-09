88ce55454be7189c124e6c6fcb5f10fa9bad1307
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-06-09 14:22:11 +0330

    refactor chat section - user and room

--- src/pages/Chat/components/Messages.jsx
+++ src/pages/Chat/components/Messages.jsx
@@ -5,50 +5,17 @@ import {
 } from "@heroicons/react/24/outline";
 import Avatar from "@/components/Avatar";
 import { useSearchParams } from "react-router-dom";
-import { useState, useEffect } from "react";
-import axios from "@/library/http";
-import { useSelector } from "react-redux";
 
-const Messages = () => {
-  const user = useSelector(state => state.auth.user);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [recieverUser, setReciverUser] = useState({});
-  const selectedUserId = searchParams.get("id");
-
-  useEffect(() => {
-    const getUser = async () => {
-      const response = await axios.get(`user/${selectedUserId}`);
-      const data = response.data;
-      setReciverUser(data);
-    };
-
-    if (!!selectedUserId && selectedUserId !== "undefined") getUser();
-  }, [selectedUserId]);
-
-  useEffect(() => {
-    const sender = user._id;
-    const reciever = selectedUserId;
-
-    const getRoom = async () => {
-      try {
-        const response = await axios.get("room", {
-          params: { sender, reciever },
-        });
-        const room = response.data;
-      } catch (err) {}
-    };
-
-    if (!!sender && !!reciever && reciever !== "undefined") {
-      console.log("hereeee");
-      getRoom();
-    }
-  }, [user._id, selectedUserId]);
+const Messages = ({ room }) => {
+  const [searchParams] = useSearchParams();
+  const recieverId = searchParams.get("id");
+  const recieverUser = room?.users?.find?.(u => u._id === recieverId);
 
   return (
     <div className="flex flex-col h-full">
       <div className="p-4 border-b-2 border-indigo-100 w-full">
         <div className="flex items-center justify-between">
-          <Avatar user={recieverUser} withDetail />
+          <Avatar user={recieverUser || {}} withDetail />
 
           <div className="flex gap-5 items-center">
             <button>
--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { useSelector } from "react-redux";
+import { useSearchParams } from "react-router-dom";
 
 import Messages from "./components/Messages";
 import SendMessage from "./components/SendMessage";
 import Sidbar from "./components/Sidbar";
 import { BASE_URL } from "@/library/config";
+import axios from "@/library/http";
 
 const Chat = () => {
-  const token = useSelector(state => state.auth.accessToken);
+  const { accessToken, user } = useSelector(state => state.auth);
+  const [searchParams] = useSearchParams();
   const [onlineUsers, setOnlineUsers] = useState([]);
+  const [room, setRoom] = useState({});
+  const selectedUserId = searchParams.get("id");
 
   useEffect(() => {
     const socket = io(BASE_URL, {
       auth: {
-        token,
+        token: accessToken,
       },
     });
 
@@ -27,6 +32,28 @@ const Chat = () => {
     });
   }, []);
 
+  useEffect(() => {
+    const sender = user._id;
+    const reciever = selectedUserId;
+
+    const getRoom = async () => {
+      try {
+        const response = await axios.get("room", {
+          params: { sender, reciever },
+        });
+
+        const room = response.data;
+        setRoom(room);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (!!sender && !!reciever && reciever !== "undefined") {
+      getRoom();
+    }
+  }, [user._id, selectedUserId]);
+
   return (
     <div className="flex w-screen h-screen">
       <div className="w-[250px] bg-indigo-50">
@@ -35,11 +62,11 @@ const Chat = () => {
 
       <div className="flex flex-col w-[calc(100%-250px)]">
         <div className="h-[calc(100%-50px)]">
-          <Messages />
+          <Messages room={room} />
         </div>
 
         <div className="h-[50px] p-4 border-t-2 border-indigo-100">
-          <SendMessage />
+          <SendMessage room={room} />
         </div>
       </div>
     </div>
